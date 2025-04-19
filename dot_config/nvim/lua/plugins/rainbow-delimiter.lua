d49b4fe655ec29154de2cf52774b90a140a8ceb6
Eric Spidle <ericspidledev@gmail.com>
2025-04-19 10:57:08 -0400

    nvim: add rainbow delimiters

---
+++ dot_config/nvim/lua/plugins/rainbow-delimiter.lua
@@ -0,0 +1,8 @@
+return {
+    "HiPhish/rainbow-delimiters.nvim",
+    config = function()
+        require('rainbow-delimiters.setup').setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+}
