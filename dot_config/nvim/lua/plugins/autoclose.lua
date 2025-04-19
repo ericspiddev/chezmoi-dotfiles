70df3d91e6bd24a7eea766b3846b6750cf5d6488
Eric Spidle <ericspidledev@gmail.com>
2025-04-19 10:34:24 -0400

    Nvim: remove autoclose

--- dot_config/nvim/lua/plugins/autoclose.lua
+++ dot_config/nvim/lua/plugins/autoclose.lua
@@ -1,11 +1,1 @@
-return {
- "m4xshen/autoclose.nvim",
-  config = function()
-        local auto_close = require("autoclose")
-        auto_close.setup({
-            keys = {
-                ["<"] = { escape = true, close =true, pair ="<>", disabled_filetypes = {}},
-            },
-        })
-    end
-}
+
