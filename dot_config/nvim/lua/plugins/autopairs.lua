add75afd95c84bf2580528295f366d16552215b8
Eric Spidle <ericspidledev@gmail.com>
2025-04-19 10:56:53 -0400

    nvim: move autclose to autopairs

---
+++ dot_config/nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,9 @@
+return {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  config = function()
+    require("nvim-autopairs").setup({
+      check_ts = true, -- use treesitter to avoid adding pairs in comments/strings
+    })
+  end,
+}
