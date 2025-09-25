8de42120dffd072bef4fcb853850e5c5b40b2019
Eric Spidle <ericspidledev@gmail.com>
2025-09-24 21:52:35 -0400

    nvim/lualine: customize components and layout

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -3,7 +3,17 @@ return {
     config = function()
         require('lualine').setup({
             options={
-                theme = 'dracula'
+                theme = 'onedark',
+                component_separators = { left = '▌', right = '▐' },
+                section_separators   = { left = '█', right = '█' },
+            },
+            sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {'filename', 'filetype'},
+                    lualine_c = {'hostname'},
+                    lualine_x = {'searchcount'},
+                    lualine_y = {'branch', 'diff'},
+                    lualine_z = {'location'}
             }
         })
     end
