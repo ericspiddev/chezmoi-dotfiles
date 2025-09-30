38731113d8fae3e5a5f0347f1e7db333661b2f3a
Eric Spidle <ericspidledev@gmail.com>
2025-09-29 22:37:49 -0400

    none: add legacy linters and lsp keybinds

--- dot_config/nvim/lua/plugins/none-ls.lua
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -1,16 +1,32 @@
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            source = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.prettier,
-                null_ls.builtins.formatting.black,
-                null_ls.builtins.formatting.isort,
-                --null_ls.builtins.diagnostics.eslint_d, somethings wrong?
-            },
-        })
-        vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
-    end
+    {
+        "nvimtools/none-ls.nvim",
+        dependencies = {
+            "nvimtools/none-ls-extras.nvim",
+        },
+    },
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            local null_lsp = require("null-ls")
+            null_lsp.setup({
+                sources = {
+                    null_lsp.builtins.formatting.stylua,
+                    null_lsp.builtins.formatting.prettier,
+                    null_lsp.builtins.formatting.black,
+                    null_lsp.builtins.formatting.isort,
+                    null_lsp.builtins.formatting.rubocop,
+                    null_lsp.builtins.diagnostics.rubocop,
+                    require('none-ls.diagnostics.eslint_d'),
+                    require('none-ls.cpplint'),
+                },
+            })
+            vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
+            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+            vim.keymap.set('n', '<leader>d', vim.lsp.buf.type_definition, {})
+            vim.keymap.set('n', '<leader>i', vim.lsp.buf.implementation, {})
+            vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, {})
+            vim.keymap.set('n', '<leader>h', vim.lsp.buf.signature_help, {})
+        end,
+    },
 }
