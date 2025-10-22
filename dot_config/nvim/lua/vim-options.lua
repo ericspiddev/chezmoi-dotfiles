ddf4b8c46a616e4c6f1479964f665b447d906049
Eric Spidle <ericspidledev@gmail.com>
2025-10-21 21:52:03 -0400

    nvim: set signs and add shortcut for noh

--- dot_config/nvim/lua/vim-options.lua
+++ dot_config/nvim/lua/vim-options.lua
@@ -6,9 +6,17 @@ vim.g.mapleader = " "
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.keymap.set('n', '<C-n>', ':tabnew <CR>', {})
+vim.keymap.set('n', 'nh', ':noh <CR>', {})
 
-local signs = { Error = " ", Warn = " ", Hint = "󰞋 ", Info = " " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+-- Set SIGN symbols for all message types
+vim.diagnostic.config({
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = ' ',
+            [vim.diagnostic.severity.WARN] = ' ',
+            [vim.diagnostic.severity.HINT] = '󰞋 ',
+            [vim.diagnostic.severity.INFO] = ' ',
+
+        }
+    }
+})
