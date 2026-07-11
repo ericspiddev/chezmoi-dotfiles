98feaec5152ed4a857dcdaa80dc3c86cde8e1e68
Eric Spidle <ericspidledev@gmail.com>
2026-07-10 20:40:33 -0400

    neovim: update keymaps

--- dot_config/nvim/lua/vim-options.lua
+++ dot_config/nvim/lua/vim-options.lua
@@ -6,9 +6,17 @@ vim.g.mapleader = " "
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.keymap.set('n', '<C-n>', ':tabnew <CR>', {})
-vim.keymap.set('n', 'nh', ':noh <CR>', {})
+vim.keymap.set('n', '<leader>nh', ':noh <CR>', {})
 vim.keymap.set('n', 'sq', ':wq <CR>', {})
-vim.keymap.set('n', 'sqa', ':wqa <CR>', {})
+vim.keymap.set('n', 'sqa', ':wqa "CR"', {})
+
+vim.keymap.set('ca', 'W', 'w', {})
+vim.keymap.set('ca', 'Wq', 'wq', {})
+vim.keymap.set('ca', 'Qa', 'qa', {})
+vim.keymap.set('ca', 'Qa!', 'qa!', {})
+vim.keymap.set('x', '<leader>p', 'P', {desc = 'Paste without yanking selection into clipboard' })
+
+vim.opt.colorcolumn = "100"
 
 -- Set SIGN symbols for all message types
 vim.diagnostic.config({
