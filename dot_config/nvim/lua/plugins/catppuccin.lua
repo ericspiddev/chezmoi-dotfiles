c0295ccaef14d9444cb1615b2a128f8ad018c4a5
Eric Spidle <ericspidledev@gmail.com>
2026-07-10 21:00:13 -0400

    tmux/nvim: minor updates

--- dot_config/nvim/lua/plugins/catppuccin.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "catppuccin/nvim",
-    config = function()
-        require("catppuccin").setup({
-            flavour=catp_flavor
-        })
-        --vim.cmd.colorscheme "catppuccin" remove for now
-        end
-}
