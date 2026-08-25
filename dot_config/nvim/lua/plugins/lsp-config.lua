597298bab9b9476e1ea359119de1f90fa80935fd
Eric Spidle <ericspidledev@gmail.com>
2026-08-24 21:30:15 -0400

    LSP: remove none-ls/completions and revamp LSP

--- dot_config/nvim/lua/plugins/lsp-config.lua
+++ dot_config/nvim/lua/plugins/lsp-config.lua
@@ -1,30 +1,39 @@
+lsps = {
+    { name = "luals",       config = {} },
+    { name = "clangd",      config = {} },
+    { name = "pylsp",        config = {}},
+    { name = "dockerls",        config = {}},
+}
+
+function lsp_register_all()
+    for _, lsp in ipairs(lsps) do
+        vim.lsp.config[lsp.name] = lsp.config
+        vim.lsp.enable(lsp.name)
+    end
+end
+
+function lsp_register_keymaps()
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+    vim.keymap.set('n', 'ca', vim.lsp.buf.code_action, {})
+    vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+    vim.keymap.set('n', '<leader>d', vim.lsp.buf.type_definition, {})
+    vim.keymap.set('n', '<leader>i', vim.lsp.buf.implementation, {})
+    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, {})
+    vim.keymap.set('n', '<leader>h', vim.lsp.buf.signature_help, {})
+end
+
 return {
     {
         "mason-org/mason.nvim",
         opts = {}
     },
     {
-        "williamboman/mason-lspconfig.nvim",
-        opts ={},
-        dependencies = {
-            { "maon-org/mason.nvim", opts = {}},
-            "neovim/nvim-lspconfig"
-        }
-    },
-    {
         "neovim/nvim-lspconfig",
         config = function()
-            vim.lsp.config['luals'] = {}
-            vim.lsp.enable('luals')
-
-            vim.lsp.config['clangd'] = {
-                filetypes = {'c', 'cpp'}
-            }
-            vim.lsp.enable('clangd')
-
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set('n', 'ca', vim.lsp.buf.code_action, {})
+            lsp_register_all()
+            lsp_register_keymaps()
         end
     }
 }
