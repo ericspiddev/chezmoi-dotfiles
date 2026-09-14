cea319570dffbab7533582419ec16391eaa40e0d
Eric Spidle <ericspidledev@gmail.com>
2026-09-13 21:13:52 -0400

    nvim: minor updates for telescope and completions

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
         vim.keymap.set('n', '<C-g>', built_in.live_grep, {})
         vim.keymap.set('n', '<leader>g', function()
             built_in.live_grep({
+                prompt_title = 'Exclude Test Files Grep',
                 additional_args = function()
                     return {
                         '--glob=!**/test/**',
@@ -19,12 +20,14 @@ return {
 
         vim.keymap.set('n', '<leader>c', function()
             built_in.find_files({
+                prompt_title = 'Find Config File',
                 cwd = vim.fn.stdpath('config'),
             })
         end, {desc = "Search for files in neovim config"})
 
         vim.keymap.set('n', '<leader>C', function()
             built_in.live_grep({
+                prompt_title = 'Grep Config File',
                 cwd = vim.fn.stdpath('config'),
             })
         end, {desc = "Grep config files"})
