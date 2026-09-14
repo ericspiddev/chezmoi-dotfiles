bec45123aa5963386141db0861631e0c9f5b0dc0
Eric Spidle <ericspidledev@gmail.com>
2026-09-13 20:43:29 -0400

    nvim/telescope: add custom pickers

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -5,5 +5,29 @@ return {
         local built_in = require("telescope.builtin")
         vim.keymap.set('n', '<C-p>', built_in.find_files, {})
         vim.keymap.set('n', '<C-g>', built_in.live_grep, {})
+        vim.keymap.set('n', '<leader>g', function()
+            built_in.live_grep({
+                additional_args = function()
+                    return {
+                        '--glob=!**/test/**',
+                        '--glob=!**/tests/**',
+                        '--glob=!*mock*',
+                    }
+                end
+                })
+        end, { desc = "Search for something in non test/mock dirs"})
+
+        vim.keymap.set('n', '<leader>c', function()
+            built_in.find_files({
+                cwd = vim.fn.stdpath('config'),
+            })
+        end, {desc = "Search for files in neovim config"})
+
+        vim.keymap.set('n', '<leader>C', function()
+            built_in.live_grep({
+                cwd = vim.fn.stdpath('config'),
+            })
+        end, {desc = "Grep config files"})
+
    end
 }
